b6f8fa3f5ee5481b151c1c01e69d2107895fb4f6
Harsh Raj <hraj20000@gmail.com>
2026-09-11 11:53:23 +0530

    feat: app constants and schedule mapping

---
+++ src/const.ts
@@ -0,0 +1,13 @@
+export const LOGIN_PATH = "/login";
+
+// Body-part session categories, in menu order.
+export const GROUP_ORDER = [
+  "Chest",
+  "Triceps",
+  "Lats",
+  "Biceps",
+  "Shoulders",
+  "Legs",
+  "Abs",
+  "Cardio",
+];
